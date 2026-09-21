f3ff9ed4678337d5ed1457368a159d19213013cd
JuanSEstupinan <u202223405@upc.edu.pe>
2026-09-21 11:40:09 -0500

    feat(layout): add Main View component JavaScript elements

---
+++ src/shared/presentation/components/layout.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import {useI18n} from "vue-i18n";
+  import {ref} from "vue";
+  import about from "../views/about.vue";
+
+  const { t } = useI18n();
+  const drawer = ref(false);
+  const toggleDrawer = () => { drawer.value = !drawer.value; }
+
+  const items = [
+    { label: 'option.home', to: '/home' },
+    { label: 'option.about', to: '/about' },
+  ];
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
