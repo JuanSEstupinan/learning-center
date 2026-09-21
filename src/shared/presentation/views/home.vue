7bb849edee04c75ecb8f23c0006f863ea7f31a0f
JuanSEstupinan <u202223405@upc.edu.pe>
2026-09-21 11:36:21 -0500

    feat(home): correction

--- src/shared/presentation/views/home.vue
+++ src/shared/presentation/views/home.vue
@@ -9,7 +9,7 @@ const { t } = useI18n();
   <section class="pt-6 p-4 md:p-5">
     <div class="flex flex-column gap-3">
       <h1 class="text-4xl font-bold text-color">{{ t('home.title') }}</h1>
-      <p class="m-0 line-height-1 text-color-secondary">{{ t('home.content') }}</p>
+      <p class="m-0 line-height-3 text-color-secondary">{{ t('home.content') }}</p>
     </div>
   </section>
 </template>
