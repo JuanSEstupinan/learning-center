1e7e309fbc103bd6219c01da8f105f4e7a6924d4
JuanSEstupinan <u202223405@upc.edu.pe>
2026-09-21 11:07:44 -0500

    chores: adds PrimeUI i18n

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Material from '@primeuix/themes/material';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import Tooltip from 'primevue/tooltip';
 
-createApp(App).mount('#app')
+const primeVueUiLicenseKey = import.meta.env.VITE_PRIME_UI_LICENSE_KEY;
+
+createApp(App)
+    .use(PrimeVue, { theme: { preset: material }, ripple: true, license: priimeUiLicenseKey })
+    .use(i18n)
+    .mount('#app')
---
+++ vite-env.d.ts
@@ -0,0 +1,3 @@
+interface ImportMetaEvn {
+    readonly VITE_
+}
