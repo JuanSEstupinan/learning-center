6abf4213b541187748aed6383c6424ae9000f228
JuanSEstupinan <u202223405@upc.edu.pe>
2026-09-21 11:12:18 -0500

    feat(in-app-navigation): Configure Router for In-App navigation

--- src/main.js
+++ src/main.js
@@ -6,10 +6,13 @@ import Material from '@primeuix/themes/material';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 import Tooltip from 'primevue/tooltip';
+import i18n from './i18n.js';
+import router from './router.js';
 
 const primeVueUiLicenseKey = import.meta.env.VITE_PRIME_UI_LICENSE_KEY;
 
 createApp(App)
     .use(PrimeVue, { theme: { preset: material }, ripple: true, license: priimeUiLicenseKey })
     .use(i18n)
+    .use(router)
     .mount('#app')
