8e452ae6be6a5a8819dba8138fc617a7954e3e06
JuanSEstupinan <u202223405@upc.edu.pe>
2026-09-21 12:01:09 -0500

    feat(layout): implement main layout component reference and structure

--- src/shared/presentation/components/layout.vue
+++ src/shared/presentation/components/layout.vue
@@ -1,6 +1,7 @@
 <script setup>
   import {useI18n} from "vue-i18n";
   import {ref} from "vue";
+  import {ConfirmDialog as PvConfirmDialog, Toast as PvToast} from "primevue";
 
   const { t } = useI18n();
   const drawer = ref(false);
@@ -10,13 +11,34 @@
     { label: 'option.home', to: '/home' },
     { label: 'option.about', to: '/about' },
   ];
-
 </script>
 
 <template>
-
+  <pv-toast/>
+  <pv-confirm-dialog/>
+  <header class="absolute top-0 left-0 w-full">
+    <pv-toolbar class="bg-primary">
+     <template #start>
+       <pv-button class="p-button-text" icon="pi pi-bars" @class="toggleDrawer"/>
+       <h3>ACME Learning Cnter</h3>
+     </template>
+     <template #end>
+       <div class="flex-column mr-3">
+         <pv-button v-for="item in items" :key="item.label" as-chil v-slot="slotProps">
+           <router-link :to="item.to" :class="slotProps['class']">{{ t(item.label) }}</router-link>
+         </pv-button>
+       </div>
+       <!-- To add when IAM is implemented -->
+       <!-- <authentication-section/> -->
+       <!-- To add when Language switcher is implemented -->
+       <!-- <language-switcher/> -->
+     </template>
+    </pv-toolbar>
+    <pv-drawer v-model="drawer"/>
+  </header>
+  <main class="mt-7">
+    <router-view/>
+  </main>
+  <!-- To add when Footer is implemented -->
+  <!-- <footer-content/>-->
 </template>
-
-<style scoped>
-
-</style>
